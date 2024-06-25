f9fa1050a234adee8605f80f964a25bb4c2d1c08
Keegan White <keeganthomaswhite@gmail.com>
2024-06-25 09:26:56 +0200

    added env file for keycloak  to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,13 +96,6 @@ WSGI_APPLICATION = "backend.wsgi.application"
 #     }
 # }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
