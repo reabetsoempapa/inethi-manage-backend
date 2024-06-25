7e49eafa5f9a8529cf2984d5fc04552b55609cdb
Keegan White <keeganthomaswhite@gmail.com>
2024-06-25 09:26:22 +0200

    added env file for keycloak

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,12 +132,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-KEYCLOAK_PUBLIC_KEY = """
------BEGIN PUBLIC KEY-----
-ENTER YOUR PUB KEY
------END PUBLIC KEY-----
-"""
-
+KEYCLOAK_PUBLIC_KEY = os.getenv('KEYCLOAK_PUBLIC_KEY')
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
