24d1bc8509ff853c923dcf1a5b5864d8a6e0af1d
Erik Polzin <eriktpol@gmail.com>
2024-08-06 21:31:11 +0200

    Use whitenoise for static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -218,6 +218,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -294,6 +295,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
